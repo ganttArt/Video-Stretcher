bda90e2de8b946d5bf2ee0b1b9d395e9a8253892
Chris Gantt <gantt.art@gmail.com>
2020-04-29 09:54:31 -0700

    Simplified stretch_all_frames function by adding a direction parameter, which eliminates the need for separate functions for stretching in each direction.

--- main.py
+++ main.py
@@ -16,28 +16,24 @@ Run
 python3 main.py
 '''
 from split_video_by_frame import split_video
-from stretch_all_frames import stretch_all_frames, upward_stretch_all_frames, stretch_all_frames_left
+from stretch_all_frames import stretch_all_frames
 from frames_to_video import create_video
 from shutil import rmtree
 from time import sleep
 
+DIRECTION_DICT = {'down': 0, 'up': 180, 'left': 90, 'right': 270}
+
 VIDEO_FILE = 'Waves_Short.mp4'
 STARTING_PIXEL = 170
-
-# 1 <= STRETCH_INTENSITY <= 13
-STRETCH_INTENSITY = 4
+STRETCH_INTENSITY = 13  # 1 <= STRETCH_INTENSITY <= 13
+DIRECTION = DIRECTION_DICT['left']  # change direction here!
 
 if __name__ == "__main__":
     FPS = split_video(VIDEO_FILE)
-
-    # choose one of these two functions to stretch up or down
-    # upward_stretch_all_frames(STARTING_PIXEL)
-    stretch_all_frames(STARTING_PIXEL)
-    # stretch_all_frames_left(STARTING_PIXEL, STRETCH_INTENSITY)
-
+    stretch_all_frames(STARTING_PIXEL, DIRECTION)
     create_video(VIDEO_FILE, FPS)
-    # rmtree('video-frames')
-    # print('video-frames directory deleted')
+    rmtree('video-frames')
+    print('video-frames directory deleted')
     rmtree('__pycache__')
     print('pycache cleared')
     print('Video stretching complete')
--- stretch_all_frames.py
+++ stretch_all_frames.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from PIL import Image
 from stretching_functions import create_np_array, create_index_list, create_gradient, build_new_image, save_file
 
-def stretch_all_frames(starting_pixel):
+def stretch_all_frames(starting_pixel=170, direction=0):
     index_list = create_index_list()
     directory = Path('video-frames').iterdir()
     number_of_frames = len(list(directory))
@@ -11,47 +11,12 @@ def stretch_all_frames(starting_pixel):
     for frame in Path('video-frames').iterdir():
         current_frame_number += 1
         with Image.open(frame) as _frame:
-            IMG_ARRAY = create_np_array(_frame)
-            NEW_IMG = build_new_image(index_list, IMG_ARRAY, starting_pixel)
-            save_file(NEW_IMG, frame)
+            _frame = _frame.rotate(direction, expand=True)
+            img_array = create_np_array(_frame)
+            new_img = build_new_image(index_list, img_array, starting_pixel)
+            new_img = new_img.rotate(-direction, expand=True)
+            save_file(new_img, frame)
             print(f'{current_frame_number}/{number_of_frames}', frame, 'stretched')
 
-
-def upward_stretch_all_frames(starting_pixel):
-    '''
-        Note: When calculating starting pixel for upward stretch,
-              Minus the starting pixel from the video height.
-    '''
-    index_list = create_index_list()
-    directory = Path('video-frames').iterdir()
-    number_of_frames = len(list(directory))
-    current_frame_number = 0
-
-    for frame in Path('video-frames').iterdir():
-        current_frame_number += 1
-        with Image.open(frame) as _frame:
-            _frame = _frame.rotate(180)
-            IMG_ARRAY = create_np_array(_frame)
-            NEW_IMG = build_new_image(index_list, IMG_ARRAY, starting_pixel)
-            NEW_IMG = NEW_IMG.rotate(180)
-            save_file(NEW_IMG, frame)
-            print(f'{current_frame_number}/{number_of_frames}', frame, 'stretched')
-
-def stretch_all_frames_left(starting_pixel, stretch_intensity=13):
-    index_list = create_index_list(stretch_intensity)
-    directory = Path('video-frames').iterdir()
-    number_of_frames = len(list(directory))
-    current_frame_number = 0
-
-    for frame in Path('video-frames').iterdir():
-        current_frame_number += 1
-        with Image.open(frame) as _frame:
-            _frame = _frame.rotate(90, expand=True)
-            IMG_ARRAY = create_np_array(_frame)
-            NEW_IMG = build_new_image(index_list, IMG_ARRAY, starting_pixel)
-            NEW_IMG = NEW_IMG.rotate(-90, expand=True)
-            save_file(NEW_IMG, frame)
-            print(f'{current_frame_number}/{number_of_frames}', frame, 'stretched')
-    
 if __name__ == "__main__":
     stretch_all_frames()
